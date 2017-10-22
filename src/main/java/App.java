5d95473366c101290124c09b4bfaa8c58a7277ff
tusenka <ourtusenka@yandex.ru>
2017-10-22 12:06:31 +0300

    To delete

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("123: Init commit");
         System.out.println("First commit");
+        System.out.println("Second commit");
     }
 }
